cdd7b5297c6d0ef32dae51b83d258adfa95e8c55
Mizuinu30 <juansilva.dvm@gmail.com>
2024-03-20 16:11:22 -0400

    Updated routers

--- my-registration-app/routes/authRoutes.js
+++ my-registration-app/routes/authRoutes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController'); // We will create this next
+const authController = require('../controllers/authController');
+const passwordResetController = require('../controllers/passwordResetController');
 
-// Route definitions
+// Regular authentication routes
+router.get('/pstart', authController.getpstart);
 router.get('/login', authController.getLogin);
 router.get('/register', authController.getRegister);
 router.post('/register', authController.postRegister);
 router.post('/login', authController.postLogin);
-// Example route for handling logout logic
 router.get('/logout', authController.logout);
+router.get('/campaignmanager', authController.getCampaignManager);
+router.get('/aboutus', authController.getAboutUS);
+
+// Password reset routes using router
+router.get('/forgot-password', passwordResetController.getForgotPassword);
+router.post('/forgot-password', passwordResetController.postForgotPassword);
+router.get('/reset-password/:token', passwordResetController.getResetPassword);
+router.post('/reset-password/:token', passwordResetController.postResetPassword);
 
 module.exports = router;
